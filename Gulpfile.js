6980b3705c0b736bc82dc9eb8fceef1e8659cfbf
Julien Bisconti <julien.bisconti@gmail.com>
2015-10-01 18:33:37 +0200

    Sass works, gulpfile improved

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,47 +1,28 @@
 var gulp = require('gulp')
 var msx = require('gulp-msx')
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 gulp.task('transform-jsx', function() {
-  return gulp.src('./app/**/*.jsx')
-    .pipe(msx({harmony: true, es6module: true}))
-    .pipe(gulp.dest('./app'))
+	return gulp.src('./app/**/*.jsx')
+	.pipe(msx({harmony: true, es6module: true}))
+	.pipe(gulp.dest('./app'))
 })
 
-// Compile and automatically prefix stylesheets
-/*
-gulp.task('styles', () => {
-	const AUTOPREFIXER_BROWSERS = [
-		'ie >= 10',
-		'ie_mob >= 10',
-		'ff >= 30',
-		'chrome >= 34',
-		'safari >= 7',
-		'opera >= 23',
-		'ios >= 7',
-		'android >= 4.4',
-		'bb >= 10'
-	];
 
-	// For best performance, don't add Sass partials to `gulp.src`
-	return gulp.src([
-		'app/!**!/!*.scss',
-		'app/!**!/!*.css'
-	])
-		.pipe($.newer({dest: '.tmp/styles', ext: '.css'}))
-		.pipe($.sourcemaps.init())
-		.pipe($.sass({
-			precision: 10
-		}).on('error', $.sass.logError))
-		.pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-		.pipe($.sourcemaps.write())
-		.pipe(gulp.dest('.tmp/styles'))
-		// Concatenate and minify styles
-		.pipe($.if('*.css', $.minifyCss()))
-		.pipe($.sourcemaps.write('.'))
-		.pipe(gulp.dest('dist/styles'))
-		.pipe($.size({title: 'styles'}));
-});
-*/
+gulp.task('sass', function () {
 
+	gulp.src('./app/**/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(concat('app.js'))
+	.pipe(sass({outputStyle: 'compressed'}))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./css'));
+})
 
-gulp.task('default', ['transform-jsx'])
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['transform-jsx','sass'])
